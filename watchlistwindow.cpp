179f29a8f237c98994a435fdd5f1436bee6ac573
greead04 <greead04@pfw.edu>
2025-09-24 20:45:07 -0400

    removed unused files

--- watchlistwindow.cpp
+++
@@ -1,8 +0,0 @@
-#include "watchlistwindow.h"
-
-WatchlistWindow::WatchlistWindow(QWidget *parent)
-    : QWidget{parent}
-{
-    testButton = new QPushButton("TEST", this);
-    testButton->show();
-}
